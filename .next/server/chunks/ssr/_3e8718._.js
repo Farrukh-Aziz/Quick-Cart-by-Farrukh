39971b86ba0cb553c5373c327d42e15c6567feb1
Farrukh Aziz <mac@macs-MacBook-Air.local>
2025-05-21 19:51:25 +0500

    Solve some eroor related to env

--- .next/server/chunks/ssr/_3e8718._.js
+++ .next/server/chunks/ssr/_3e8718._.js
@@ -1066,7 +1066,7 @@ const Footer = ()=>{
             }, this),
             /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$ssr$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$ssr$5d$__$28$ecmascript$29$__["jsxDEV"])("p", {
                 className: "py-4 text-center text-xs md:text-sm",
-                children: "Copyright 2025 © GreatStack.dev All Right Reserved."
+                children: "Copyright 2025 © Farrukh-Aziz All Right Reserved."
             }, void 0, false, {
                 fileName: "[project]/components/Footer.jsx",
                 lineNumber: 49,
--- .next/static/chunks/_6281da._.js
+++ .next/static/chunks/_6281da._.js
@@ -1140,7 +1140,7 @@ const Footer = ()=>{
             }, this),
             /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$compiled$2f$react$2f$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$client$5d$__$28$ecmascript$29$__["jsxDEV"])("p", {
                 className: "py-4 text-center text-xs md:text-sm",
-                children: "Copyright 2025 © GreatStack.dev All Right Reserved."
+                children: "Copyright 2025 © Farrukh-Aziz All Right Reserved."
             }, void 0, false, {
                 fileName: "[project]/components/Footer.jsx",
                 lineNumber: 49,
